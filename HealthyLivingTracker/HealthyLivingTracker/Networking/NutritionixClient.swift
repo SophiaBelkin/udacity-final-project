58e705f038d8fc0ccd71c5e8464dd92efb934d04
SophiaBelkin <sophia.lu@belkin.com>
2021-10-04 10:31:18 -0700

    Added initial files

---
+++ HealthyLivingTracker/HealthyLivingTracker/Networking/NutritionixClient.swift
@@ -0,0 +1,19 @@
+//
+//  NutritionixClient.swift
+//  HealthyLivingTracker
+//
+//  Created by Sophia Lu on 10/3/21.
+//
+
+import Foundation
+import Alamofire
+
+class NutritionixClient {
+    static let apiKey = ""
+    
+    enum Endpoints {
+        static let base = ""
+        
+        
+    }
+}
---
+++ HealthyLivingTracker/HealthyLivingTracker/ViewController/DashboardViewController.swift
@@ -0,0 +1,18 @@
+//
+//  DashboardViewController.swift
+//  HealthyLivingTracker
+//
+//  Created by Sophia Lu on 10/3/21.
+//
+
+import UIKit
+
+class DashboardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
